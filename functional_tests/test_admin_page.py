1f0838debe5ab55c79e4f5ac1446855aa979f458
n-freman <smiron1703@gmail.com>
2023-11-11 13:02:27 +0500

    fix functional tests; attach models to admin page

--- functional_tests/test_admin_page.py
+++ functional_tests/test_admin_page.py
@@ -1,9 +1,13 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.contrib.auth import get_user_model
+import time
 
+from django.contrib.auth import get_user_model
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 class AdminPageTest(StaticLiveServerTestCase):
@@ -30,8 +34,17 @@ class AdminPageTest(StaticLiveServerTestCase):
         password_input.send_keys('admin')
         self.browser.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
 
-    def test_models_attached_to_admin_page(self):
-        self.assertIn(self.browser.page_source, 'Song')
-        self.assertIn(self.browser.page_source, 'Singer')
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
+    def test_models_attached_to_admin_page(self):
+        self.wait_for(lambda: self.assertIn('Song', self.browser.page_source))
+        self.wait_for(lambda: self.assertIn('Singer', self.browser.page_source))
 
